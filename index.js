9c396fc23dc6c7b133b2980ddd82aaec98c9d474
Kevin Mahoney <kmahoney@theatlantic.com>
2019-08-30 11:23:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import {translate, toneIsCorrect} from 'translate-utils';
+
+const print = async () => {
+ 
+    console.log(await translate('lettuce'));
+    console.log(toneIsCorrect([1,4],'生菜'))
+}
+
+print()
