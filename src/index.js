f28983061ab373f9c095a558a154c8395e693c1e
Kevin Mahoney <kmahoney@theatlantic.com>
2019-11-25 17:04:53 -0500

    render canvas and key movement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import { init } from 'kontra';
+import { init, Sprite, GameLoop, initKeys, keyPressed } from "kontra";
+import { reduce } from "rxjs/operator/reduce";
+import { settings } from "./settings";
 
 const { canvas, context } = init();
+console.log(context)
+initKeys();
 
+const person = Sprite({
+  x: 0,
+  y: 175,
+  color: reduce,
+  width: 20,
+  height: 40
+  // dx: 2
+});
 
+const road = Sprite({
+  x: 0,
+  y: 175,
+  width: 800,
+  height: 60,
+  color: "brown"
+});
 
+const gameButton = document.getElementById("game-button");
+gameButton.addEventListener("click", () => {
+  person.x += 10;
+});
 
+const movePlayerListener = () => {
+  if (keyPressed("right")) {
+    person.x += settings.playerSpeed;
+  }
+  if (keyPressed("left")) {
+    person.x -= settings.playerSpeed;
+  }
+};
 
+const loop = GameLoop({
+  update: () => {
+    movePlayerListener();
+  },
+  render: () => {
+    road.render();
+    person.render();
+  }
+});
 
-
+loop.start();
---
+++ src/settings.js
@@ -0,0 +1,4 @@
+export const settings = {
+    playerSpeed: 2
+}
+
